e000a8f1de504e8c60cfbfa53a2f314fe0be478c
eslam3400 <eslam.magdy@twerlo.com>
2022-09-02 14:17:47 +0200

    set default route

--- index.js
+++ index.js
@@ -2,4 +2,6 @@ const EXPRESS = require('express')
 const APP = express()
 const PORT = process.env.PORT || 3000
 
+APP.get("/", (req, res) => req.send("<h1>Hello World</h1>"))
+
 APP.listen(PORT, () => console.log(`server running on http://localhost:${PORT}`))
