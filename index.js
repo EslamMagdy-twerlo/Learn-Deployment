fe29e5051d3bf8ed62053ad6875062cbaf8db490
eslam3400 <eslam.magdy@twerlo.com>
2022-09-02 14:25:45 +0200

    fix bug

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const EXPRESS = require('express')
-const APP = express()
+const APP = EXPRESS()
 const PORT = process.env.PORT || 3000
 
-APP.get("/", (req, res) => req.send("<h1>Hello World</h1>"))
+APP.get("/", (req, res) => res.send("<h1>Hello World</h1>"))
 
 APP.listen(PORT, () => console.log(`server running on http://localhost:${PORT}`))
